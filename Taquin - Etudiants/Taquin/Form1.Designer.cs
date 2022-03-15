b35ce076aa906558e84785ef1ed008ac3afd3e2e
Anijiberschi <arthur.nijiberschi@helb-prigogine.be>
2022-03-15 21:05:31 +0100

    a

---
+++ Taquin - Etudiants/Taquin/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Taquin
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.bt_start = new System.Windows.Forms.Button();
+            this.bt_quitter = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeColumns = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.ColumnHeadersHeight = 29;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.grid.ColumnHeadersVisible = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.grid.DefaultCellStyle = dataGridViewCellStyle1;
+            this.grid.Enabled = false;
+            this.grid.Location = new System.Drawing.Point(87, 69);
+            this.grid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.grid.MultiSelect = false;
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.RowHeadersVisible = false;
+            this.grid.RowHeadersWidth = 51;
+            this.grid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.grid.Size = new System.Drawing.Size(672, 583);
+            this.grid.TabIndex = 0;
+            this.grid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellClick);
+            // 
+            // bt_start
+            // 
+            this.bt_start.Location = new System.Drawing.Point(202, 722);
+            this.bt_start.Name = "bt_start";
+            this.bt_start.Size = new System.Drawing.Size(75, 23);
+            this.bt_start.TabIndex = 1;
+            this.bt_start.Text = "Start";
+            this.bt_start.UseVisualStyleBackColor = true;
+            this.bt_start.Click += new System.EventHandler(this.bt_start_Click);
+            // 
+            // bt_quitter
+            // 
+            this.bt_quitter.Location = new System.Drawing.Point(520, 722);
+            this.bt_quitter.Name = "bt_quitter";
+            this.bt_quitter.Size = new System.Drawing.Size(75, 23);
+            this.bt_quitter.TabIndex = 2;
+            this.bt_quitter.Text = "Quitter";
+            this.bt_quitter.UseVisualStyleBackColor = true;
+            this.bt_quitter.Click += new System.EventHandler(this.bt_quitter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 769);
+            this.Controls.Add(this.bt_quitter);
+            this.Controls.Add(this.bt_start);
+            this.Controls.Add(this.grid);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+        private System.Windows.Forms.DataGridView grid;
+        private System.Windows.Forms.Button bt_start;
+        private System.Windows.Forms.Button bt_quitter;
+    }
+}
+
---
+++ Taquin - Etudiants/Taquin/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taquin
+{
+	public partial class Form1 : Form
+	{
+		private const int MAX_ROW = 4;
+		private const int MAX_COL = 4;
+		private int aRow = 0, aCol = 0;
+		public Random random = new Random();
+		private int[,] matrice = new int[MAX_ROW, MAX_COL];
+
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void createGrid()
+		{
+			DataTable dt = new DataTable();
+
+			for (int row = 0; row < MAX_ROW; row++)
+				dt.Rows.Add();
+
+			for (int col = 0; col < MAX_COL; col++)
+				dt.Columns.Add();
+
+			grid.DataSource = dt;
+
+			for (int col = 0; col < MAX_COL; col++)
+				grid.Columns[col].Width = (grid.Width / MAX_COL);
+
+			for (int row = 0; row < MAX_ROW; row++)
+				grid.Rows[row].Height = grid.Height / MAX_ROW;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			createGrid();
+		}
+
+        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+			int row = e.RowIndex;
+			int col = e.ColumnIndex;
+			int tempo =0;
+
+			if (grid.Rows[e.RowIndex].Cells[e.ColumnIndex].Style.BackColor == Color.Orange)
+            {
+				aRow = e.RowIndex;
+				aCol= e.ColumnIndex;
+				
+				
+            }
+            else
+            {
+				if ((e.ColumnIndex + e.RowIndex) - (aCol + aRow) == 1 || (aCol + aRow) - (e.ColumnIndex + e.RowIndex) == 1)
+                {
+					tempo = matrice[aRow, aCol];
+					matrice[aRow, aCol] = matrice[e.RowIndex, e.ColumnIndex];
+					grid.Rows[aRow].Cells[aCol].Value = matrice[aRow, aCol];
+					coloriage(aRow, aCol);
+					matrice[e.RowIndex, e.ColumnIndex] = tempo;
+					grid.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = matrice[e.RowIndex, e.ColumnIndex];
+					coloriage(e.RowIndex, e.ColumnIndex);
+					if (isWin())
+                    {
+						MessageBox.Show("C'est gagné");
+						grid.Enabled = false;
+                    }
+				}
+				
+			}
+
+			
+
+		}
+
+		public void fillMatrice()
+        {
+			for (int i=0; i<MAX_ROW; i++)
+            {
+				for (int j=0; j<MAX_COL; j++)
+                {
+					matrice[i, j] = j +1+ i*MAX_COL;
+                }
+            }
+			matrice[MAX_COL-1, MAX_ROW-1] = (MAX_COL*MAX_ROW);
+		}
+
+		public void coloriage(int row, int col)
+        {
+			if (matrice[row,col] == MAX_COL * MAX_ROW)
+            {
+				grid.Rows[row].Cells[col].Style.BackColor = Color.White;
+				grid.Rows[row].Cells[col].Style.ForeColor= Color.White;
+			}
+            else
+            {
+				grid.Rows[row].Cells[col].Style.BackColor = Color.Orange;
+				grid.Rows[row].Cells[col].Style.ForeColor = Color.Black;
+			}
+        }
+
+		public void fillGrid()
+        {
+			for (int i=0;i < MAX_COL * MAX_ROW; i++)
+            {
+				grid.Rows[i/MAX_ROW].Cells[i%MAX_COL].Value=matrice[i/MAX_ROW,i%MAX_COL];
+				grid.Rows[i / MAX_ROW].Cells[i % MAX_COL].Style.BackColor = Color.Orange;
+				grid.Rows[i / MAX_ROW].Cells[i % MAX_COL].Style.ForeColor= Color.Black;
+				
+				
+				if (matrice[i / MAX_ROW, i % MAX_COL] == (MAX_COL * MAX_ROW))
+				{
+					grid.Rows[i / MAX_ROW].Cells[i % MAX_COL].Style.BackColor = Color.White;
+					grid.Rows[i / MAX_ROW].Cells[i % MAX_COL].Style.ForeColor = Color.White;
+				}
+			}
+
+		}
+
+		public bool isWin()
+        {
+			bool flag = true;
+			int cpt = 0;
+            while (flag && cpt < MAX_ROW*MAX_COL)
+            {
+				flag = (matrice[cpt / MAX_ROW, cpt % MAX_COL] == cpt+1);
+				cpt++;
+			}
+			return flag;
+		}
+
+
+		public void melange()
+        {
+
+			for (int i=0;i<100;i++)
+            {
+				int case1 = random.Next(MAX_COL * MAX_ROW);
+				int case2 = random.Next(MAX_COL * MAX_ROW);
+				int temp;
+
+				temp = matrice[case1/MAX_COL,case1%MAX_ROW];
+				matrice[case1 / MAX_COL, case1 % MAX_ROW] = matrice[case2 / MAX_COL, case2 % MAX_ROW];
+				matrice[case2 / MAX_COL, case2 % MAX_ROW] = temp;
+			}
+		}
+
+        private void bt_start_Click(object sender, EventArgs e)
+        {
+			grid.Enabled =true;
+			fillMatrice();
+			melange();
+			fillGrid();
+        }
+
+        private void bt_quitter_Click(object sender, EventArgs e)
+        {
+			Close();
+        }
+    }
+}
