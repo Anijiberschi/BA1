002b81c5c7261bef90fca370a8bd18e09eb32afa
Anijiberschi <arthur.nijiberschi@helb-prigogine.be>
2022-03-21 12:58:25 +0100

    Puissance4

---
+++ Puissance4 - Etudiants/Puissance4 - Etudiants/Puissance4/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Puissance4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grid = new System.Windows.Forms.DataGridView();
+            this.bStart = new System.Windows.Forms.Button();
+            this.bQuit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToDeleteRows = false;
+            this.grid.AllowUserToResizeColumns = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.ColumnHeadersHeight = 29;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.grid.ColumnHeadersVisible = false;
+            this.grid.Enabled = false;
+            this.grid.Location = new System.Drawing.Point(131, 31);
+            this.grid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.grid.MultiSelect = false;
+            this.grid.Name = "grid";
+            this.grid.ReadOnly = true;
+            this.grid.RowHeadersVisible = false;
+            this.grid.RowHeadersWidth = 51;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.grid.Size = new System.Drawing.Size(529, 402);
+            this.grid.TabIndex = 0;
+            this.grid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grid_CellClick);
+            // 
+            // bStart
+            // 
+            this.bStart.Location = new System.Drawing.Point(199, 484);
+            this.bStart.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bStart.Name = "bStart";
+            this.bStart.Size = new System.Drawing.Size(125, 62);
+            this.bStart.TabIndex = 1;
+            this.bStart.Text = "Commencer";
+            this.bStart.UseVisualStyleBackColor = true;
+            this.bStart.Click += new System.EventHandler(this.bStart_Click);
+            // 
+            // bQuit
+            // 
+            this.bQuit.Location = new System.Drawing.Point(465, 484);
+            this.bQuit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.bQuit.Name = "bQuit";
+            this.bQuit.Size = new System.Drawing.Size(125, 62);
+            this.bQuit.TabIndex = 2;
+            this.bQuit.Text = "Quitter";
+            this.bQuit.UseVisualStyleBackColor = true;
+            this.bQuit.Click += new System.EventHandler(this.bQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 614);
+            this.Controls.Add(this.bQuit);
+            this.Controls.Add(this.bStart);
+            this.Controls.Add(this.grid);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView grid;
+		private System.Windows.Forms.Button bStart;
+		private System.Windows.Forms.Button bQuit;
+    }
+}
+
---
+++ Puissance4 - Etudiants/Puissance4 - Etudiants/Puissance4/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Puissance4
+{
+    public partial class Form1 : Form
+    {
+        private const int MAX_ROW = 6;
+        private const int MAX_COL = 7;
+        private Bitmap[] tabImage = new Bitmap[3];
+        private int[,] matrice = new int[6, 7];
+        private int[] JetonsCol = new int[MAX_COL];
+        private int jouer_actif = 0;
+        private int NbJoueur = 2;
+        
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void initTabImage()
+        {
+            tabImage[0] = new Bitmap("red circle.ico");
+            tabImage[1] = new Bitmap("black circle.ico");
+            tabImage[2] = new Bitmap(1, 1);
+        }
+
+        private void createGame()
+        {
+            initTabImage();
+
+            emptyGrid();
+
+
+            grid.ClearSelection();
+            grid.Enabled = true;
+        }
+
+        private void createGrid()
+        {
+            DataTable dt = new DataTable();
+            DataGridViewImageColumn imageCol = null;
+
+            for (int row = 0; row < MAX_ROW; row++)
+                dt.Rows.Add();
+
+            for (int col = 0; col < MAX_COL; col++)
+            {
+                dt.Columns.Add();
+                imageCol = new DataGridViewImageColumn();
+                grid.Columns.Add(imageCol);
+            }
+
+            grid.DataSource = dt;
+
+            for (int col = 0; col < MAX_COL; col++)
+                grid.Columns[col].Width = (grid.Width / MAX_COL);
+
+            for (int row = 0; row < MAX_ROW; row++)
+                grid.Rows[row].Height = grid.Height / MAX_ROW;
+        }
+
+        private void emptyGrid()
+        {
+            for (int row = 0; row < MAX_ROW; row++)
+            {
+                for (int col = 0; col < MAX_COL; col++)
+                {
+                    grid.Rows[row].Cells[col].Style.BackColor = Color.Wheat;
+                    grid.Rows[row].Cells[col].Value = tabImage[2];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createGrid();
+            createGame();
+            grid.Enabled = false;
+        }
+
+        private void grid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int col = e.ColumnIndex;
+
+            PosInMat(col);
+
+        }
+
+        private void PosInMat(int col)
+        {
+
+            int row = (MAX_ROW-1) - JetonsCol[col];
+
+           if (row > -1)
+            {
+                matrice[row, col] = jouer_actif;
+                JetonsCol[col]++;
+                ChangeJoueur();
+                TabToGrid(row, col);
+                
+            }
+            
+        }
+
+
+        private void checkHorizontal(int row, int col)
+        {
+            if (col == 0)
+            {
+                
+            }
+
+        private void ChangeJoueur()
+        {
+            jouer_actif = (jouer_actif + 1) % NbJoueur;
+        }
+
+        private void TabToGrid(int row, int col)
+        {
+            grid.Rows[row].Cells[col].Value = tabImage[jouer_actif];
+        }
+
+        private void bStart_Click(object sender, EventArgs e)
+        {
+            grid.Enabled = true;
+            
+        }
+
+        private void bQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
